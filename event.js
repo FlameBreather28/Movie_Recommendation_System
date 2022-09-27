96fc4d2f0fdc7827217986bb7cf4121a84e627a9
root <root@Sushant.localdomain>
2022-09-27 20:06:14 +0530

    Added event handler to first movie card and made a prototype interface of overall detail of first movie card

---
+++ event.js
@@ -0,0 +1,17 @@
+var hell = document.getElementsByClassName('movie-cards');
+
+        hell[0].addEventListener('click', () => {
+            
+            // if (hell[0]) {
+                h_1();
+                
+               
+            // }
+        });
+
+
+
+function h_1() {
+    console.log("hwllo)")
+    window.location='HollywoodMovies/h-1/h-1.html'
+}
